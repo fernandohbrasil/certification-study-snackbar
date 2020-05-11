7f9337105a23ec8bb6a7f5a979278ded9859497d
fernandohbrasil <fernandohbrasil@hotmail.com>
2020-05-11 23:02:02 +0100

    full project

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# certification-study-snackbar
-App created in order to explore the possibilities of Android Snackbar.
+# Certification Study SnackBar
+
+This application is part of a series of Apps that I am developing following the Studies guide of [Associate Android Developer](https://developers.google.com/certification/associate-android-developer).
+
+## Motivation 
+
+The purpose of this application was to explore some possibilities that [SnackBar](https://developer.android.com/reference/com/google/android/material/snackbar/Snackbar) have.
+
+## Content
+
+The following methods were covered
+
+* B addCallback (BaseCallback<B> callback)
+* void dismiss()
+* Snackbar setAnimationMode(int animationMode)
+* Snackbar setActionTextColor (int color)
+* Snackbar setAction (CharSequence text, View.OnClickListener listener)
+* Snackbar setBackgroundTint (int color)
+* Snackbar setTextColor (int color)
+* void show()
+
+## Screenshots
+
+![Record](screenshots/record.gif)
+
+## Final
+
+This App is really simple, and was focused just in [SnackBar](https://developer.android.com/reference/com/google/android/material/snackbar/Snackbar), so no specific architecture was observed. Feel free to use this example to start your studys, change the code and add in your repository as well.
+
+Any question, bug or request. Open an issue!
--- app/src/main/java/com/fernando/certificationstudysnackbar/MainActivity.kt
+++ app/src/main/java/com/fernando/certificationstudysnackbar/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.fernando.certificationstudysnackbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.fernando.certificationstudysnackbar.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
+
+private const val snack_hidden = "Your SnackBar is hidden.."
+private const val snack_visible = "Your SnackBar is visible.."
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var snackbar: Snackbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lblTitle.text = snack_hidden
+
+        binding.apply {
+            btnShow.setOnClickListener { showClick() }
+        }
+    }
+
+    private fun getMessage(): String {
+        if (binding.edtMessage.text.toString().isNotEmpty()) {
+            return binding.edtMessage.text.toString()
+        }
+        return "You didn't type anything!"
+    }
+
+    private fun getAnimationMode(): Int {
+        if (binding.rbFade.isChecked) {
+            return Snackbar.ANIMATION_MODE_FADE
+        }
+        return Snackbar.ANIMATION_MODE_SLIDE
+    }
+
+    private fun getDuration(): Int {
+        return when {
+            binding.rbLong.isChecked -> Snackbar.LENGTH_LONG
+            binding.rbShort.isChecked -> Snackbar.LENGTH_SHORT
+            else -> Snackbar.LENGTH_INDEFINITE
+        }
+    }
+
+    private fun getBackground(): Int {
+        return when {
+            binding.rbBackRed.isChecked -> R.color.red
+            binding.rbBackBlue.isChecked -> R.color.blue
+            else -> R.color.yellow
+        }
+    }
+
+    private fun getTextColor(): Int {
+        return when {
+            binding.rbTextGreen.isChecked -> R.color.green
+            binding.rbTextOrange.isChecked -> R.color.orange
+            else -> R.color.purple
+        }
+    }
+
+    private fun getActionColor(): Int {
+        return when {
+            binding.rbActionBlack.isChecked -> R.color.black
+            else -> R.color.white
+        }
+    }
+
+    private val callBack = object : BaseTransientBottomBar.BaseCallback<Snackbar>() {
+        override fun onShown(transientBottomBar: Snackbar?) {
+            super.onShown(transientBottomBar)
+            binding.lblTitle.text = snack_visible
+        }
+
+        override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+            super.onDismissed(transientBottomBar, event)
+            binding.lblTitle.text = snack_hidden
+        }
+    }
+
+    private fun showClick() {
+        snackbar = Snackbar.make(binding.lblContent, getMessage(), getDuration())
+
+        snackbar.setAction("Cancel Snack") { snackbar.dismiss() }
+        snackbar.setBackgroundTint(ContextCompat.getColor(this, getBackground()))
+        snackbar.setTextColor(ContextCompat.getColor(this, getTextColor()))
+        snackbar.setActionTextColor(ContextCompat.getColor(this, getActionColor()))
+
+
+        snackbar.addCallback(callBack)
+
+        snackbar.animationMode = getAnimationMode()
+
+        snackbar.show()
     }
 }
